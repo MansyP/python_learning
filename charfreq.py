1ea725d3181be8eb47f02c197763b01f88b8becc
MansyP <52779247+MansyP@users.noreply.github.com>
2019-08-11 10:39:21 -0700

    Create charfreq.py

---
+++ charfreq.py
@@ -0,0 +1,13 @@
+def fre(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+a=str(input(""))
+print(fre(a))
+
+
